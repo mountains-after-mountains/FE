ad681ae9eea7952c1d3b947927962212d9b883a9
gyulhana <matearz@korea.ac.kr>
2024-07-08 10:49:29 +0900

    feat: 공통 탭 컴포넌트 구현

--- src/pages/mountain/index.tsx
+++ src/pages/mountain/index.tsx
@@ -1,9 +1,24 @@
+import CommonTabs from '@/components/common/CommonTabs'
 import { useParams } from 'react-router-dom'
 
 const Mountain = () => {
   const { mountainId } = useParams()
+  const tab1 = {
+    text: '상세 정보',
+    value: 'details',
+    content: <></>,
+  }
+  const tab2 = {
+    text: '후기',
+    value: 'review',
+    content: <></>,
+  }
 
-  return <div>{mountainId}</div>
+  return (
+    <div>
+      <CommonTabs tab1={tab1} tab2={tab2} />
+    </div>
+  )
 }
 
 export default Mountain
