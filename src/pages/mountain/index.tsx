5cbd16eb9a1d3d1aa4e6192246f5e4ef868fa0ac
gyulhana <matearz@korea.ac.kr>
2024-07-08 18:41:27 +0900

    design: 산 정보 상세 페이지 상단 구현

--- src/pages/mountain/index.tsx
+++ src/pages/mountain/index.tsx
@@ -1,21 +1,28 @@
 import CommonTabs from '@/components/common/CommonTabs'
 import { useParams } from 'react-router-dom'
+import DetailMountainInfo from './components/DetailMountainInfo'
 
 const Mountain = () => {
   const { mountainId } = useParams()
   const tab1 = {
     text: '상세 정보',
     value: 'details',
-    content: <></>,
+    content: (
+      <div className="p-5">
+        <div>코스</div>
+      </div>
+    ),
   }
   const tab2 = {
     text: '후기',
     value: 'review',
-    content: <></>,
+    content: <div>abcjd</div>,
   }
 
   return (
     <div>
+      <DetailMountainInfo />
+      <div className="mt-5 h-2 w-full bg-gray-100" />
       <CommonTabs tab1={tab1} tab2={tab2} />
     </div>
   )
