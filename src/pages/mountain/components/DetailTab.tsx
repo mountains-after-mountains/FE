e09ecb8320a730617a2aa80298171e600099b085
gyulhana <matearz@korea.ac.kr>
2024-07-22 22:45:05 +0900

    feat: 지도 컴포넌트 목업 데이터 반영

--- src/pages/mountain/components/DetailTab.tsx
+++ src/pages/mountain/components/DetailTab.tsx
@@ -1,12 +1,37 @@
 import CourseCard from './CourseCard'
 import Flag from '@/assets/icons/flag.svg?react'
+import Map from './Map'
 
 const DetailTab = () => {
+  const markers = [
+    {
+      title: '카카오',
+      lat: 33.450705,
+      lng: 126.570677,
+    },
+    {
+      title: '생태연못',
+      lat: 33.450936,
+      lng: 126.569477,
+    },
+    {
+      title: '텃밭',
+      lat: 33.450879,
+      lng: 126.56994,
+    },
+    {
+      title: '근린공원',
+      lat: 33.451393,
+      lng: 126.570738,
+    },
+  ]
+
   return (
     <>
       <div className="p-5">
         <span className="text-h5 text-gray-900">코스</span>
       </div>
+      <Map lat={33.450701} lng={126.570667} markers={markers} />
       <div className="p-5">
         <div className="mb-[10px] flex items-center">
           <Flag />
--- src/pages/mountain/index.tsx
+++ src/pages/mountain/index.tsx
@@ -1,27 +1,16 @@
-import CommonTabs from '@/components/common/CommonTabs'
 import { useParams } from 'react-router-dom'
 import DetailMountainInfo from './components/DetailMountainInfo'
 import DetailTab from './components/DetailTab'
-import ReviewTab from './components/ReviewTab'
+import Divider from '@/components/common/Divider'
 
 const Mountain = () => {
   const { mountainId } = useParams()
-  const tab1 = {
-    text: '상세 정보',
-    value: 'details',
-    content: <DetailTab />,
-  }
-  const tab2 = {
-    text: '후기',
-    value: 'review',
-    content: <ReviewTab />,
-  }
 
   return (
     <div>
       <DetailMountainInfo />
-      <div className="mt-5 h-2 w-full bg-gray-100" />
-      <CommonTabs tab1={tab1} tab2={tab2} />
+      <Divider />
+      <DetailTab />
     </div>
   )
 }
