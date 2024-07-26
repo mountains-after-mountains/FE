2decc117a0d124a4f99fb8ac50c52b6caad488b2
황재연 <hjng0825@gmail.com>
2024-07-26 16:08:36 +0900

    feat : memo Drawer 추가

--- src/pages/schedule/detail/index.tsx
+++ src/pages/schedule/detail/index.tsx
@@ -1,12 +1,12 @@
-import { EditIcon } from '@/icons'
 import MemoDescription from '@/pages/schedule/detail/components/MemoDescription.tsx'
+import MemoDrawer from '@/pages/schedule/detail/components/MemoDrawer.tsx'
 
 const DetailSchedule = () => {
   return (
     <div className="p-4">
       <div className="flex items-center justify-between">
         <div className="text-h5">메모장</div>
-        <EditIcon />
+        <MemoDrawer />
       </div>
       <div className="pt-4">
         <MemoDescription />
