d73a13bc558138066782eb41e9aa6ea3061d7ede
황재연 <hjng0825@gmail.com>
2024-07-29 17:59:19 +0900

    feat : 메모장 ui 구현

--- src/pages/schedule/detail/components/MemoDrawer.tsx
+++ src/pages/schedule/detail/components/MemoDrawer.tsx
@@ -1,7 +1,27 @@
+import { useState } from 'react'
 import { Drawer, DrawerContent, DrawerFooter, DrawerHeader, DrawerTitle, DrawerTrigger } from '@/components/ui/drawer'
 import { EditIcon, WasteBasket } from '@/icons'
+import FooterButton from '@/components/common/button/FooterButton.tsx'
 
+interface ChecklistItem {
+  id: number
+  text: string
+}
 const MemoDrawer = () => {
+  const [checklistItems, setChecklistItems] = useState<ChecklistItem[]>([{ id: 1, text: '' }])
+
+  const addChecklistItem = () => {
+    setChecklistItems([...checklistItems, { id: checklistItems.length + 1, text: '' }])
+  }
+
+  const removeChecklistItem = (id: number) => {
+    setChecklistItems(checklistItems.filter(item => item.id !== id))
+  }
+
+  const handleInputChange = (id: number, value: string) => {
+    setChecklistItems(checklistItems.map(item => (item.id === id ? { ...item, text: value } : item)))
+  }
+
   return (
     <Drawer>
       <DrawerTrigger>
@@ -15,14 +35,27 @@ const MemoDrawer = () => {
           </DrawerTitle>
         </DrawerHeader>
         <div className="px-4">
-          <div className="flex gap-1">
-            <input className="w-full rounded border-2 px-4 py-[13.5px] focus:outline-none" />
-            <button className="rounded border-2 border-green-400 p-[14px]">
-              <WasteBasket />
-            </button>
-          </div>
+          {checklistItems.map(item => (
+            <div key={item.id} className="mb-2 flex gap-1">
+              <input
+                className="w-full rounded border-2 px-4 py-[13.5px] focus:outline-none"
+                value={item.text}
+                onChange={e => handleInputChange(item.id, e.target.value)}
+              />
+              <button
+                className="rounded border-2 border-green-400 p-[14px]"
+                onClick={() => removeChecklistItem(item.id)}
+              >
+                <WasteBasket />
+              </button>
+            </div>
+          ))}
         </div>
-        <DrawerFooter></DrawerFooter>
+        <DrawerFooter className="pt-20">
+          <FooterButton variant="bright" onClick={addChecklistItem}>
+            체크리스트({checklistItems.length}/50) 추가하기 +
+          </FooterButton>
+        </DrawerFooter>
       </DrawerContent>
     </Drawer>
   )
