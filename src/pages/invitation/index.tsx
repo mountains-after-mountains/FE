d1a91310ac882ce5665d34a392be691e075639f9
황재연 <hjng0825@gmail.com>
2024-08-22 13:53:18 +0900

    feat : url 복사 기능 추가

--- src/pages/invitation/index.tsx
+++ src/pages/invitation/index.tsx
@@ -11,6 +11,7 @@ const Invitation = () => {
   const { invitationId } = useParams<{ invitationId: string }>()
   const { data, isFetching } = useInvitationData(invitationId || '')
   const { formattedDate, dDayText } = useDateInfo(data?.scheduleDate)
+  const invitationUrl = `https://over-the-mountain.site/invitation/accept/${data?.invitationId}`
 
   return (
     <div className="flex flex-col">
@@ -35,9 +36,9 @@ const Invitation = () => {
             title={`${dDayText} ${formattedDate} ${data?.mountainName} ${data?.courseName}`}
             description={data?.text}
             imageUrl="https://mud-kage.kakao.com/dn/NTmhS/btqfEUdFAUf/FjKzkZsnoeE4o19klTOVI1/openlink_640x640s.jpg"
-            webUrl={`https://over-the-mountain.site/invitation/accept/${data?.invitationId}`}
+            webUrl={invitationUrl}
           />
-          <UrlShareButton />
+          <UrlShareButton url={invitationUrl} />
         </div>
       </div>
     </div>
